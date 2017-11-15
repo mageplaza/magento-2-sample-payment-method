a1da3265f59374b1472dfee280cb5bcab1774b0f
Sam <sam@mageplaza.com>
2017-11-15 16:03:16 +0700

    Update Magento sample payment gateway

--- registration.php
+++ registration.php
@@ -3,8 +3,9 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mageplaza_Payment',
+    'Magento_SamplePaymentGateway',
     __DIR__
 );
